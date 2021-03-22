0d4b1bb01cc83c16df5ced6cc61112dd32b61c61
Lucy Gilbert <lucygilbert01@gmail.com>
2021-03-22 00:53:26 +0000

    menu for interview notes

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ var PageLinks = {
   CONTACT: 'contactLink',
 };
 
+function initialLoad() {
+  var pageQuery = window.location.search.match('page=([a-zA-Z]*)&*');
+  var page = pageQuery && pageQuery[1];
+  return changePage(page || Pages.ABOUT);
+}
+
 function changePage(selectedPage) {
   var navCheck = document.getElementById('nav-check');
   navCheck.checked = false;
   Object.keys(Pages).forEach(function (key) {
     var page = document.getElementById(Pages[key]);
+    if (page == null) return;
     var pageLink = document.getElementById(PageLinks[key]);
     var displayType;
 
